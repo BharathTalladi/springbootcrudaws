c9547047a9db3e4db62c6ac2a8ae2096d3bd2918
Bharath Talladi <bharaththalladi@gmail.com>
2023-05-09 15:54:47 -0500

    Initial Version

---
+++ src/main/java/com/usecase/springbootcrudaws/SpringbootcrudawsApplication.java
@@ -0,0 +1,13 @@
+package com.usecase.springbootcrudaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootcrudawsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootcrudawsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/usecase/springbootcrudaws/SpringbootcrudawsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usecase.springbootcrudaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootcrudawsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
